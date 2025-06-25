f0c96c97b988959e66ab6fd84068ce67f5006439
tabassumNowshin <nowshintabassum429@gmail.com>
2025-06-25 11:42:33 +0600

    added testing API and Unknown route

--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@ import { app } from "./app";
 require("dotenv").config()
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`server is connected  ${process.env.PORT}`)
+    console.log(`server is connected port  ${process.env.PORT}`)
 })
