c2e60d3ba73102f8096bd041fd1d7f6528664405
tabassumNowshin <nowshintabassum429@gmail.com>
2025-06-28 20:16:24 +0600

    Drag the Middleware and tsconfig folder into server folder

---
+++ middleware/Error.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../Utils/ErrorHandler";
+            
+module.exports = (err: any, req: Request, res: Response, next: NextFunction) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'Internal server error';
+
+    // Wrong Mongoose Object ID Error (CastError)
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Duplicate key error (MongoDB)
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong JWT error
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'JSON Web Token is invalid. Try Again!';
+        err = new ErrorHandler(message, 400);
+    }
+
+    // JWT Expired error
+    if (err.name === 'TokenExpiredError') {
+        const message = 'JSON Web Token is expired. Try Again!';
+        err = new ErrorHandler(message, 400);
+    }
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+    });
+};
