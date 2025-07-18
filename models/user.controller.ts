8c5624451220a768ccfdced123c271cea175fb7c
tabassumNowshin <nowshintabassum429@gmail.com>
2025-07-18 17:44:26 +0600

    added login user,activateuser to usercontroller and jwt.ts file to utils folder

--- models/user.controller.ts
+++ models/user.controller.ts
@@ -126,4 +126,34 @@ export const activateUser = catchAsyncErrors(async(req:Request,res:Response,next
         return next(new ErrorHandler(error.message,400))
     }
 })
+
+//login user
+ interface ILoginRequest {
+    email: string;
+    password: string;
+ }
+ export const loginUser = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body as ILoginRequest;
+
+    if (!email || !password) {
+      return next(new ErrorHandler("Please enter email and password", 400));
+    }
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    }
+
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    }
+
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+});
+
         
--- models/user.model.ts
+++ models/user.model.ts
@@ -1,5 +1,8 @@
+require ('dotenv').config();    
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
+import Jwt from 'jsonwebtoken';
+
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,6 +18,8 @@ export interface IUser extends Document {
     isVerified: boolean;
     courses: Array<{ courseId: string }>;
     comparePassword: (password: string) => Promise<boolean>;
+    SignAccessToken: () => string;
+    SignRefreshToken: ()=> string;
 }
 
 const userSchema: Schema<IUser> = new mongoose.Schema({
@@ -66,6 +71,16 @@ userSchema.pre<IUser>("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
+
+//sign access token 
+userSchema.methods.SignAccessToken = function (){
+    return Jwt.sign({id: this._id},process.env.ACCESS_TOKEN || ' ')
+
+}
+userSchema.methods.SignRefreshToken = function (){
+    return Jwt.sign({id: this._id},process.env.REFRESH_TOKEN || ' ')
+
+}
 
 // Compare password method
 userSchema.methods.comparePassword = async function (enteredPassword: string): Promise<boolean> {
