ac166082a66432fa4a6adedff53a2e482bf3d083
tabassumNowshin <nowshintabassum429@gmail.com>
2025-07-21 21:12:01 +0600

    added get single course without purchasing

--- models/course.controller.ts
+++ models/course.controller.ts
@@ -66,3 +66,20 @@ export const editCourse = catchAsyncErrors(
         }
     }
 );
+// get single course ---- without purchasing
+export const getSingleCourse = catchAsyncErrors(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const course = await CourseModel.findById(req.params.id).select(
+                "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+            );
+            
+            res.status(200).json({
+                success: true,
+                course,
+            });
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 500));
+        }
+    }
+);
--- routes/course.route.ts
+++ routes/course.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { editCourse, uploadCourse } from "../models/course.controller";
+import { editCourse, getSingleCourse, uploadCourse } from "../models/course.controller";
 import { authorizeRoles, IsAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
 courseRouter.post("/create-course", IsAuthenticated, authorizeRoles("admin"), uploadCourse)
 
 courseRouter.put("/edit-course/:id", IsAuthenticated, authorizeRoles("admin"), editCourse)
+courseRouter.get("/get-course/:id", getSingleCourse)
 
 export default courseRouter;
